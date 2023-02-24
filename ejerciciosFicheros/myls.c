76a4fa4626c810a3c39593e1c3a648a3d09b6fa1
aluque1 <aluquevillegas@gmail.com>
2023-02-24 12:35:54 +0100

    finished

--- ejerciciosFicheros/myls.c
+++ ejerciciosFicheros/myls.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <getopt.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string.h>
@@ -9,14 +10,20 @@
 
 extern char *optarg;
 
+int get_size_dir(char *dname, size_t *blocks);
+int do_ls(char *path, struct stat *stat_buff);
+
 int main(int argc, char *argv[])
 {
-	char buf[PATH_MAX];
 	int opt;
 	char *path;
+	char *name;
+	DIR *dir;
 	struct stat *stat_buff = malloc(sizeof(struct stat));
+	struct stat *temp = malloc(sizeof(struct stat));
+	struct dirent *buff = malloc(sizeof(struct dirent));
 
-	while ((opt = getopt(argc, argv, "d:f:")) != -1)
+	while ((opt = getopt(argc, argv, "hd:f:")) != -1)
 	{
 		{
 			switch (opt)
@@ -24,45 +31,59 @@ int main(int argc, char *argv[])
 			case 'd':
 				path = optarg;
 				lstat(path, stat_buff);
-				if (S_ISREG(stat_buff->st_mode) == 1)
+				if (S_ISDIR(stat_buff->st_mode) == 1 && (stat_buff->st_mode & (S_IRUSR | S_IXUSR)) )
 				{
-					printf("%s (%f kB, %d link)", basename(path), stat_buff->st_size / 1024, stat_buff->st_nlink);
-					if (stat_buff->st_mode & S_IXUSR)
+					if ((dir = opendir(path)) != NULL)
 					{
-						printf("*\n");
+						while ((buff = readdir(dir)) != NULL)
+						{
+							name = buff->d_name;
+							lstat(name, temp);
+							do_ls(name, temp);
+						}
 					}
-					else
-					{
-						printf("\n");
-					}
 				}
-				else if (S_ISLNK(stat_buff->st_mode) == 1)
-				{
-					printf("%s (%s)\n", basename(path), realpath(path, buf));
-				}
-				else if (S_ISDIR(stat_buff->st_mode) == 1)
-				{
-					printf("[%s] (%d link)\n", basename(path), stat_buff->st_nlink);
-				}
+				break;
 
 			case 'f':
+				path = optarg;
+				lstat(path, stat_buff);
 				if (S_ISREG(stat_buff->st_mode) == 1)
 				{
-					printf("%s (inodo %d, %f kB )", basename(path), stat_buff->st_ino, stat_buff->st_size / 1024);
-					if (stat_buff->st_mode & S_IXUSR)
-					{
-						printf("*\n");
-					}
-					else
-					{
-						printf("\n");
-					}
+					do_ls(path, stat_buff);
 				}
+				break;
 			default:
 				fprintf(stderr, "Usage: %s [-d ruta_directorio][-f ruta_fichero_regular]\n", argv[0]);
 				exit(EXIT_FAILURE);
+				break;
 			}
 		}
 		return 0;
+	}
+}
+
+int do_ls(char *path, struct stat *stat_buff)
+{
+	char buf[PATH_MAX];
+	if (S_ISREG(stat_buff->st_mode) == 1)
+	{
+		printf("%s (%0.2f kB, %d link)", path, (float)(stat_buff->st_size >> 10), stat_buff->st_nlink);
+		if (stat_buff->st_mode & S_IXUSR)
+		{
+			printf("*\n");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+	else if (S_ISLNK(stat_buff->st_mode) == 1)
+	{
+		printf("%s (%s)\n", path, realpath(path, buf));
+	}
+	else if (S_ISDIR(stat_buff->st_mode) == 1)
+	{
+		printf("[%s] (%d link)\n", path, stat_buff->st_nlink);
 	}
 }
