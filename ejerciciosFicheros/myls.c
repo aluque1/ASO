5eb046b2b4072cc2dd8eefd113b8d7b37448984c
aluque1 <aluquevillegas@gmail.com>
2023-02-24 09:57:59 +0100

    no funciona

--- ejerciciosFicheros/myls.c
+++ ejerciciosFicheros/myls.c
@@ -5,63 +5,64 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <libgen.h>
-#include <limits.h> 
+#include <limits.h>
 
 extern char *optarg;
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	char buf[PATH_MAX];
 	int opt;
 	char *path;
 	struct stat *stat_buff = malloc(sizeof(struct stat));
 
-	while ((opt = getopt(argc, argv, "df")))
+	while ((opt = getopt(argc, argv, "d:f:")) != -1)
 	{
-		switch (opt)
 		{
-		case 'd':
-			path = optarg;
-			lstat(path, stat_buff);
-			if (S_ISREG(stat_buff->st_mode) == 1)
+			switch (opt)
 			{
-				printf("%s (%f kB, %d link)", basename(path), stat_buff->st_size / 1024, stat_buff->st_nlink);
-				if (stat_buff->st_mode & S_IXUSR)
+			case 'd':
+				path = optarg;
+				lstat(path, stat_buff);
+				if (S_ISREG(stat_buff->st_mode) == 1)
 				{
-					printf("*\n");
+					printf("%s (%f kB, %d link)", basename(path), stat_buff->st_size / 1024, stat_buff->st_nlink);
+					if (stat_buff->st_mode & S_IXUSR)
+					{
+						printf("*\n");
+					}
+					else
+					{
+						printf("\n");
+					}
 				}
-				else
+				else if (S_ISLNK(stat_buff->st_mode) == 1)
 				{
-					printf("\n");
+					printf("%s (%s)\n", basename(path), realpath(path, buf));
 				}
-			}
-			else if (S_ISLNK(stat_buff->st_mode) == 1)
-			{
-				printf("%s (%s)\n", basename(path), realpath(path, buf));
-			}
-			else if (S_ISDIR(stat_buff->st_mode) == 1)
-			{
-				printf("[%s] (%d link)\n", basename(path), stat_buff->st_nlink);
-			}
+				else if (S_ISDIR(stat_buff->st_mode) == 1)
+				{
+					printf("[%s] (%d link)\n", basename(path), stat_buff->st_nlink);
+				}
 
-		case 'f':
-			if (S_ISREG(stat_buff->st_mode) == 1)
-			{
-				printf("%s (inodo %d, %f kB )", basename(path), stat_buff->st_ino ,stat_buff->st_size / 1024);
-				if (stat_buff->st_mode & S_IXUSR)
+			case 'f':
+				if (S_ISREG(stat_buff->st_mode) == 1)
 				{
-					printf("*\n");
+					printf("%s (inodo %d, %f kB )", basename(path), stat_buff->st_ino, stat_buff->st_size / 1024);
+					if (stat_buff->st_mode & S_IXUSR)
+					{
+						printf("*\n");
+					}
+					else
+					{
+						printf("\n");
+					}
 				}
-				else
-				{
-					printf("\n");
-				}
+			default:
+				fprintf(stderr, "Usage: %s [-d ruta_directorio][-f ruta_fichero_regular]\n", argv[0]);
+				exit(EXIT_FAILURE);
 			}
-			path = optarg;
-		default:
-			fprintf(stderr, "Usage: %s [-d ruta_directorio][-f ruta_fichero_regular]\n", argv[0]);
-			exit(EXIT_FAILURE);
 		}
+		return 0;
 	}
-	return 0;
 }
