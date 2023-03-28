4f59efffd110c82f28e842a8867f424ad153bcae
aluque1 <aluquevillegas@gmail.com>
2023-03-28 12:22:28 +0200

    inicio de ejercicio 2 de pipes

--- ejerciciosFicheros/pipe1_3.c
+++ ejerciciosFicheros/pipe1_3.c
@@ -1,5 +1,81 @@
-int main(int argc, char const *argv[])
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define TIMEOUT 30
+#define BUF_LEN 1024
+
+int main(void)
 {
-    /* code */
-    return 0;
+    
+    struct timeval tv;
+    fd_set readfds;
+    int ret;
+    char buf[BUF_LEN + 1];
+    int len;
+
+    FD_ZERO(&readfds);
+
+    /* Wait up to five seconds. */
+    tv.tv_sec = TIMEOUT;
+    tv.tv_usec = 0;
+
+    ret = select(STDIN_FILENO + 1, &readfds, NULL, NULL, &tv);
+
+    /*
+    ret: numero de fds preparados para I/O (puede ser 0 si timeout), -1 si error, ejemplo: EINTR (interrupcion)
+    */
+    if (ret == -1)
+    {
+        perror("select");
+        return 1;
+    }
+    else if (!ret)
+    {
+        printf("Han pasado %d segundos.\n", TIMEOUT);
+        return 0;
+    }
+
+    /*
+        read from the two pipes named tuberia2 and mituberia with select to see if something has
+        been written in them
+    */
+
+    int tuberia2 = open("tuberia2", O_NONBLOCK);
+    int mituberia = open("mituberia", O_NONBLOCK);
+    if (FD_ISSET(tuberia2, &readfds))
+    {
+        len = read(tuberia2, buf, BUF_LEN);
+        if (len == -1)
+        {
+            perror("read");
+            return 1;
+        }
+        if (len)
+        {
+            buf[len] = '\0';
+            printf("Hemos leido de la tuberia2: %s\n", buf);
+        }
+        return 0;
+    }
+    if (FD_ISSET(mituberia, &readfds))
+    {
+        len = read(mituberia, buf, BUF_LEN);
+        if (len == -1)
+        {
+            perror("read");
+            return 1;
+        }
+        if (len)
+        {
+            buf[len] = '\0';
+            printf("Hemos leido de la mituberia: %s\n", buf);
+        }
+        return 0;
+    }
+    fprintf(stderr, "se ha producido un error!\n");
+    return 1;
 }
---
+++ ejerciciosFicheros/tuberia_sin_nombre.c
@@ -0,0 +1,60 @@
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    int pipefd[2];
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe");
+        return EXIT_FAILURE;
+    }
+
+    int pid1 = fork();
+    if (pid1 < 0)
+    {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+    else if (pid1 == 0)
+    {
+        // Hijo 1 (comando de argv[2])
+        // rerouting of stdout
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]); // cerramos un extremo
+        close(pipefd[1]); // cerramos el otro extremo creado por dupe
+        // execlp 
+    }
+
+    // como usamos exec no hace falta if para el padre
+    
+    int pid2 = fork();
+    if (pid2 < 0)
+    {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+    else if (pid2 == 0)
+    {
+        // Hijo 2 (comando de argv[3])
+        // rerouting of stdin
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]); // cerramos un extremo
+        close(pipefd[1]); // cerramos el otro extremo creado por dupe
+        // execlp 
+    }
+
+    close(pipefd[0]); // cerramos un extremo
+    close(pipefd[1]); // cerramos el otro extremo
+    
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    return 0;
+}
